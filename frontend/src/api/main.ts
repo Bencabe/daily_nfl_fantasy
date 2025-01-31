9722bd1ae241ec4729ecd8ce1509b43dd661e268
Ben <bencabe93@gmail.com>
2025-01-31 13:18:00 +0000

    get correct token on api calls

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -1,9 +1,13 @@
 import { EplFantasy } from "./openapi/EplFantasy";
 
+const getToken = () => {
+  return document.cookie.split('jwt_token=')[1] || localStorage.getItem('jwt_token');
+};
+
 const api = new EplFantasy({
     BASE: import.meta.env.VITE_API_URL || 'http://localhost:5001',
     HEADERS: {
-      'Authorization': `Bearer ${document.cookie.split('jwt_token=')[1]}`,
+      'Authorization': `Bearer ${getToken()}`,
       'Cookie': document.cookie,
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
